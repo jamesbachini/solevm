fef923910c3313c6f334b32d51d47de5083fc63c
James Bachini <james@mcambridge.com>
2021-05-05 10:02:08 +0100

    check owner on addValidator

--- solevm/consensus/validators.sol
+++ solevm/consensus/validators.sol
@@ -1,10 +1,19 @@
 pragma solidity ^0.8.1;
 
 contract ManageValidators {
+
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
     address[] public validators;
     
     function addValidator(address _addy) public {
-        validators.push(_addy);
+        if (msg.sender == owner) {
+            validators.push(_addy);
+        }
     }
     
     function validatorCount() public view returns (uint) {
