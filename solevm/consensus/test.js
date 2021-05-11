0394dec15bd028920957d80c199ffedc8a5e227c
James Bachini <james@mcambridge.com>
2021-05-11 23:11:39 +0100

    add validators

--- solevm/consensus/test.js
+++ solevm/consensus/test.js
@@ -11,7 +11,7 @@ const solanaJSON = require('./solana-consensus.js');
 		}
 		const app = await solanaJSON.loadProgram(connection,smartContract,payerAccount);
 		console.log('app',app);
-		const confirmationTicket = await solanaJSON.pushJSON(connection,app,'{"abc":123}');
+		const confirmationTicket = await solanaJSON.pushJSON(connection,app,'{"validators":["0x04Fec2d572497397007FD96E431156698F3abb14","0x2F41ad391A5BC0E994CcB6ccbfCfF67b9D9515AC"]}');
 		const testJSON = solanaJSON.pullJSON(connection,app.appAccount.publicKey);
 		console.log(`Test: ${JSON.parse(testJSON).abc}`);
 })();
